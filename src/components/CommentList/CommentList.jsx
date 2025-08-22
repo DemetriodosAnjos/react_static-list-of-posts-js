065292da88c76ad5416ef69c6fdd720a4d352794
Demetrio dos Anjos <ddoanjos@gmail.com>
2025-08-22 17:34:36 -0300

    Renomeada const user para postUser

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,1 +1,14 @@
-export const CommentList = () => <>Put the list here</>;
+import React from 'react';
+
+import { CommentInfo } from '../CommentInfo/CommentInfo'; // Vamos criar este em seguida
+
+export const CommentList = ({ comments }) => {
+  return (
+    <div className="CommentList">
+      <h3>Comments</h3>
+      {comments.map(comment => (
+        <CommentInfo key={comment.id} comment={comment} />
+      ))}
+    </div>
+  );
+};
